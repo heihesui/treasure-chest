6e23257dc07b8997f6d711e50958e58809a1e4de
charlessui <charles.sui@grapecity.com>
2023-12-30 16:07:28 +0800

    init

---
+++ src/main/java/org/example/binarysearch/BinarySearch.java
@@ -0,0 +1,42 @@
+package org.example.binarysearch;
+
+public class BinarySearch {
+    // 二分查找的递归实现
+    public int bsearch1(int[] a, int n, int val) {
+        return bsearchInternally(a, 0, n - 1, val);
+    }
+
+    private int bsearchInternally(int[] a, int low, int high, int value) {
+        if (low > high) return -1;
+
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return bsearchInternally(a, mid + 1, high, value);
+        } else {
+            return bsearchInternally(a, low, mid - 1, value);
+        }
+    }
+
+    /**
+     * 二分查找的循环写法
+     */
+    public int bsearch2(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
